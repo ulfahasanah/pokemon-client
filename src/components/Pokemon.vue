0e5297e43ce68c4512f6489b60bdc0aa674449d0
ulfa hasanah <ulfa2912@outlook.com>
2021-10-25 11:41:17 +0700

    feat(stats): add vue google charts for pokemon stats

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,17 +4,19 @@
             <loading/>
         </div>
         <div v-if="!isLoading">
-            <b-card no-body class="mx-auto overflow-hidden mt-5" style="max-width: 360px;">
+            <b-card no-body class="mx-auto overflow-hidden mt-4">
                 <b-row no-gutters>
-                <b-col md="6">
-                    <b-card-img :src="pokemon.sprites.front_default" alt="Image" class="rounded-0"></b-card-img>
-                </b-col>
-                <b-col md="6">
+                <b-col md="7" class="text-center mx-auto">
                     <b-card-body :title="pokemon.name">
-                    <b-card-text>
-                        This is a wider card with supporting text as a natural lead-in to additional content.
-                        This content is a little bit longer.
-                    </b-card-text>
+                    <b-card-img :src="pokemon.sprites.front_default" alt="Image" class="rounded-0"></b-card-img>
+                     <GChart
+                        type="BarChart"
+                        :settings="{packages: ['bar']}"
+                        :data="pokemon.stats"
+                        :options="chartOptions"
+                        :createChart="(el, google) => new google.charts.Bar(el)"
+                        @ready="onChartReady"
+                    />
                     </b-card-body>
                 </b-col>
                 </b-row>
@@ -26,16 +28,20 @@
 <script>
 import axios from 'axios' 
 import loading from '../components/Loading.vue'
+import { GChart } from 'vue-google-charts'
+
 
 export default {
     data() {
         return {
             pokemon: {},
-            isLoading: true
+            isLoading: true,
+            chartsLib: null
         }
     },
     components: {
-        loading
+        loading,
+        GChart
     },
     created: function () {
         this.getRandomPokemon();
@@ -48,9 +54,38 @@ export default {
             Authorization: `Bearer ${accessToken}`
             }
         });
-        console.log(res.data, "===")
         this.pokemon = res.data
+        console.log(res.data);
+        let temp = [[], []]
+        this.pokemon.stats.forEach(el => {
+            temp[1].push(el.base_stat) 
+        });
+        this.pokemon.stats.forEach(el => {
+            temp[0].push(el.stat.name) 
+        });
+        this.pokemon.stats = temp
+        const temp1 = ['stat performance'].concat(temp[0])
+        const temp2 = ['stat'].concat(temp[1])
+        this.pokemon.stats = [temp1, temp2]
+
         this.isLoading = false
+        },
+         onChartReady (chart, google) {
+            this.chartsLib = google
+        },
+    },
+    computed: {
+        chartOptions () {
+        if (!this.chartsLib) return null
+        return this.chartsLib.charts.Bar.convertOptions({
+            chart: {
+            title: 'Stat Performance',
+            },
+            bars: 'vertical', // Required for Material Bar Charts.
+            hAxis: { format: 'decimal' },
+            height: 450,
+            width: 350
+            })
         }
     }
 }
